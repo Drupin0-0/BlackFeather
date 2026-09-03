c38f8ec877106be28ea70c0f48b534013e9dcde7
Eduardo <linseduardo2009@gmail.com>
2026-09-03 11:52:05 -0300

    feat: Reegisterview

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .serializers import UserSerializer
+from rest_framework import status
+from rest_framework.views import APIView
 # Create your views here.
+
+
+class RegisterView(APIView):
+    def RegisterView(APIView):
+        def post(self, request):
+            serializer = UserSerializer(data=request.data)
+
+            if serializer.is_valid():
+                return 
